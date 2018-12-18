66dbcf3a8962c4e33fc78a2b3d5563e01ef046c4
skjerp <semak.alexander@gmail.com>
2018-12-18 14:44:14 +0200

    add cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cors = require('cors');
 
 const keys = require('./config/keys');
 const routes = require('./routes/index');
@@ -9,7 +10,7 @@ const routes = require('./routes/index');
 
 const passportConfig = require('./config/passport');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9000;
 
 mongoose
   .connect(keys.dbUri, { useNewUrlParser: true })
@@ -18,6 +19,8 @@ mongoose
 
 
 const app = new Express();
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
